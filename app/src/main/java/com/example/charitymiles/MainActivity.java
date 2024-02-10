fc69c89a8ae5c50a7748afa60744cfdac97c7af2
yashd <yashdoctor2000@gmail.com>
2024-02-10 13:27:57 -0600

    Login Page is ready with reference in back-end as well.

--- app/src/main/java/com/example/charitymiles/MainActivity.java
+++ app/src/main/java/com/example/charitymiles/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.charitymiles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private Button signUpButton;
+    private TextView forgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login_button);
+        signUpButton = findViewById(R.id.signup_button);
+        forgotPassword = findViewById(R.id.forgot_password);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
