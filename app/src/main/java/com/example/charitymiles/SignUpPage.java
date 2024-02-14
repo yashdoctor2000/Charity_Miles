de9b077ee18e16f67d161773e24a9c05ff6793b4
yashd <yashdoctor2000@gmail.com>
2024-02-13 22:15:17 -0600

    Firebase integration done.

--- app/src/main/java/com/example/charitymiles/SignUpPage.java
+++ app/src/main/java/com/example/charitymiles/SignUpPage.java
@@ -1,14 +1,62 @@
 package com.example.charitymiles;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpPage extends AppCompatActivity {
 
+    private EditText nameEditText,addressEditText,mobileNumberEditText,passwordEditText;
+    private Button signUpButton;
+    FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+
+//        firestore = FirebaseFirestore.getInstance();
+//        Map<String,Object> user =new HashMap<>();
+//        user.put("firstname","Yash");
+//        user.put("Lastname","Doctor");
+//
+//        firestore.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//            @Override
+//            public void onSuccess(DocumentReference documentReference) {
+//                Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(getApplicationContext(),"Failure",Toast.LENGTH_LONG).show();
+//            }
+//        });
+
+        nameEditText = findViewById(R.id.nameEditText);
+        addressEditText = findViewById(R.id.addressEditText);
+        mobileNumberEditText = findViewById(R.id.mobileNumberEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        signUpButton = findViewById(R.id.signUpButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
     }
 }
