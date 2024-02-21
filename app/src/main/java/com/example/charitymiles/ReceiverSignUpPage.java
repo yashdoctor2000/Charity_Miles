5c8a4108133f235f23f67bfb8c1c7aa30a3f2c85
yashdoctor2000 <yashdoctor.180410116021@gmail.com>
2024-02-20 21:31:17 -0600

    New Activity added for Receiver signup page

---
+++ app/src/main/java/com/example/charitymiles/ReceiverSignUpPage.java
@@ -0,0 +1,14 @@
+package com.example.charitymiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceiverSignUpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiver_sign_up_page);
+    }
+}
