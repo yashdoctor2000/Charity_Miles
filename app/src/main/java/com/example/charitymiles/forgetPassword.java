29e47907434f6b5c809c4e4c5214041ab64f8a28
yashdoctor2000 <yashdoctor.180410116021@gmail.com>
2024-03-11 11:34:42 -0500

    forget password page front end and back end ready

--- app/src/main/java/com/example/charitymiles/forgetPassword.java
+++ app/src/main/java/com/example/charitymiles/forgetPassword.java
@@ -1,14 +1,71 @@
 package com.example.charitymiles;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class forgetPassword extends AppCompatActivity {
+    private Button btnReset,btnBack;
+    private EditText edtEmail;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+    private String strEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+
+        btnBack = findViewById(R.id.btnForgotPasswordBack);
+        btnReset=findViewById(R.id.btnReset);
+        edtEmail=findViewById(R.id.edtForgotPasswordEmail);
+        progressBar=findViewById(R.id.forgetPasswordProgressbar);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                strEmail=edtEmail.getText().toString().trim();
+                if(!TextUtils.isEmpty(strEmail)){
+                    ResetPassword();
+                }
+                else{
+                    edtEmail.setError("Email field can't be empty");
+                }
+            }
+        });
+    }
+
+    private void ResetPassword() {
+        progressBar.setVisibility(View.VISIBLE);
+        btnReset.setVisibility(View.INVISIBLE);
+        mAuth.sendPasswordResetEmail(strEmail)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(forgetPassword.this,"Reset Password link has been sent to your registered Email",Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility(View.INVISIBLE);
+                        btnReset.setVisibility(View.VISIBLE);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(forgetPassword.this,"Reset Password link has not been sent",Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility(View.INVISIBLE);
+                        btnReset.setVisibility(View.VISIBLE);
+                    }
+                });
     }
 }
