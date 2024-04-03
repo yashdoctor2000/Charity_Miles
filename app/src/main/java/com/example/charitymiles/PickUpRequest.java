7a4a8107db6d089ab52714a7018228d27992de26
yashdoctor2000 <yashdoctor.180410116021@gmail.com>
2024-04-03 16:48:20 -0500

    Slight change

--- app/src/main/java/com/example/charitymiles/PickUpRequest.java
+++ app/src/main/java/com/example/charitymiles/PickUpRequest.java
@@ -25,7 +25,7 @@ public class PickUpRequest extends AppCompatActivity {
     private EditText etPreferredDate, etPreferredTime, etAdditionalInformation;
     private FirebaseAuth mAuth;
     private Button btnSchedulePickup;
-    private DatabaseReference myRef;
+    private DatabaseReference myRef, myRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +42,7 @@ public class PickUpRequest extends AppCompatActivity {
         etAdditionalInformation = findViewById(R.id.etAdditionalInformation);
         mAuth = FirebaseAuth.getInstance();
         myRef = FirebaseDatabase.getInstance().getReference("Donations");
+        myRequest = FirebaseDatabase.getInstance().getReference("Pick-UP-Request");
 
         OrganizationModel organization = (OrganizationModel) getIntent().getSerializableExtra("OrganizationForPickup");
 
@@ -65,6 +66,7 @@ public class PickUpRequest extends AppCompatActivity {
                 final String time = etPreferredTime.getText().toString().trim();
                 final String addInfo = etAdditionalInformation.getText().toString().trim();
                 final String OrgId = Uid;
+                final int status = 1;
 
                 if (desAddress.isEmpty() || donationQuantity.isEmpty() || donationItem.isEmpty() || date.isEmpty() || time.isEmpty()) {
                     Toast.makeText(PickUpRequest.this, "All fields are required.", Toast.LENGTH_SHORT).show();
@@ -75,6 +77,7 @@ public class PickUpRequest extends AppCompatActivity {
                 if(Donor != null){
                     String DonorId = Donor.getUid();
                     Map<String, Object> donation = new HashMap<>();
+                    Map<String, Object> request = new HashMap<>();
                     donation.put("OrgName", OrgName);
                     donation.put("desAddress", desAddress);
                     donation.put("donationItem",donationItem);
@@ -85,13 +88,28 @@ public class PickUpRequest extends AppCompatActivity {
                     donation.put("DonorId",DonorId);
                     donation.put("AddInfo",addInfo);
 
+                    request.put("OrgName", OrgName);
+                    request.put("desAddress", desAddress);
+                    request.put("donationItem",donationItem);
+                    request.put("donationQuantity", donationQuantity);
+                    request.put("date",date);
+                    request.put("time",time);
+                    request.put("OrgId",OrgId);
+                    request.put("DonorId",DonorId);
+                    request.put("AddInfo",addInfo);
+                    request.put("IsStatus", status);
+
                     myRef.push().setValue(donation).addOnSuccessListener(aVoid ->{
-                        Toast.makeText(PickUpRequest.this, "Donation Requested", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(PickUpRequest.this,DonorFinal.class);
-                        startActivity(intent);
+
 
                     }).addOnFailureListener(aVoid ->{
                         Toast.makeText(PickUpRequest.this, "Failed", Toast.LENGTH_SHORT).show();
+                    });
+
+                    myRequest.push().setValue(request).addOnSuccessListener(aVoid ->{
+                        Toast.makeText(PickUpRequest.this, "Donation Requested", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(PickUpRequest.this,DonorFinal.class);
+                        startActivity(intent);
                     });
 
                 }
